5b8ff912f4ef16ae067e5980c396caad84616627
Emilin Syju <w0847462@myscc.ca>
2024-11-06 14:40:52 -0500

    Created DAO for Property Type table

---
+++ src/main/java/dao/PropertyTypeDAO.java
@@ -0,0 +1,10 @@
+package dao;
+
+import java.util.ArrayList;
+
+public interface PropertyTypeDAO {
+    public ArrayList<PropertyType> getAllPropertyTypes();
+    public PropertyType getTypeId(int type_id);
+    public PropertyType getTypeName(int type_name);
+
+}
