f2d80f7a31a0868d4d71fe1f9de43e2242f69046
Emilin Syju <w0847462@myscc.ca>
2024-11-08 18:12:46 -0500

    #31 Created more methods for DAO's

--- src/main/java/Dao/PropertyTypeDAO.java
+++ src/main/java/Dao/PropertyTypeDAO.java
@@ -1,5 +1,6 @@
 package Dao;
 
+import com.example.propertypro.Pojo.ClientPOJO;
 import com.example.propertypro.Pojo.PropertyTypePOJO;
 
 import java.util.ArrayList;
@@ -7,5 +8,8 @@ import java.util.ArrayList;
 public interface PropertyTypeDAO {
     public ArrayList<PropertyTypePOJO> getAllPropertyTypes();
     public PropertyTypePOJO getPropertyType(int property_type_id);
+    public void deletePropertyType(int property_type_id);
+    public void updatePropertyType(PropertyTypePOJO propertyType);
+    public void createPropertyType(PropertyTypePOJO propertyType);
 
 }
