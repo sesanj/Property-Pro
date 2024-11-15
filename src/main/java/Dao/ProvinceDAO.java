aea2b64dcf3ec7b7b148ac67c95bb233700c3ee6
“Aarav <aaravabrahamofficial@gmail.com>
2024-11-15 17:37:27 -0500

    #31 Completed Pending Query methods Final

--- src/main/java/Dao/ProvinceDAO.java
+++ src/main/java/Dao/ProvinceDAO.java
@@ -1,6 +1,5 @@
 package Dao;
 
-import com.example.propertypro.Pojo.PropertyTypePOJO;
 import com.example.propertypro.Pojo.ProvincePOJO;
 
 import java.util.ArrayList;
@@ -8,7 +7,7 @@ import java.util.ArrayList;
 public interface ProvinceDAO{
     public ArrayList<ProvincePOJO> getAllProvinces();
     public ProvincePOJO getProvince(int province_id );
-    public void deleteProvince(int province_id);
+    public ProvincePOJO deleteProvince(int province_id);
     public void updateProvince(ProvincePOJO province);
     public void createProvince(ProvincePOJO province);
 
